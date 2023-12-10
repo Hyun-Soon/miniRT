4562b9d6cf8515c0ccf4b8955e56942268a190c8
Hyun-Soon <s7514036@naver.com>
2023-12-11 00:03:36 +0900

    oh no

--- src/trace/hit/hit.c
+++ src/trace/hit/hit.c
@@ -14,7 +14,19 @@ t_bool hit(t_object *world, t_ray *ray, t_hit_record *rec)
 			hit_anything = TRUE;
 			temp_rec.tmax = temp_rec.t;
 			*rec = temp_rec;
-			//here
 		}
+		world = world->next;
 	}
+	return (hit_anything);
+}
+
+//hit obj는 오브젝트 타입에 맞는 hit함수로 연결해주는 관문
+t_bool	hit_obj(t_object *world, t_ray *ray, t_hit_record *rec)
+{
+	t_bool	hit_result;
+
+	hit_result = FALSE;
+	if (world->type == SP)
+		hit_result = hit_sphere(world, ray, rec);
+	return (hit_result);
 }
